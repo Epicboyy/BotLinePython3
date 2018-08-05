ff629599f740cdd70c4898d59b7522a4fbda657c
Epicboyy <41615146+Epicboyy@users.noreply.github.com>
2018-08-05 15:28:58 +0800

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -27,8 +27,8 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[96]
-    APP_VER     = '8.0.2'
+    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
+    APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
     SYSTEM_NAME = 'Nadya'
     SYSTEM_VER  = '11.2.5'
